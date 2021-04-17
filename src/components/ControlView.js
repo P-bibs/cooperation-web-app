b4ade96b8792915327036bf05ac8de26b7e79655
Paul Biberstein <biberstein.paul@gmail.com>
2021-04-17 13:49:45 -0400

    Remove extra files and add first component

---
+++ src/components/ControlView.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class ControlView extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+        <div>ControlView</div>
+      </>
+    );
+  }
+}
