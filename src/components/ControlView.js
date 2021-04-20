3cc2f345529ee767c08f408f29001652040b81d5
Paul Biberstein <biberstein.paul@gmail.com>
2021-04-20 15:47:50 -0400

    Add other instrument names

--- src/components/ControlView.js
+++ src/components/ControlView.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+let zip = (a1, a2) => a1.map((x, i) => [x, a2[i]]);
+
 const ControlView = (props) => {
   const updatePattern = (indexToSwap) => {
     const newPattern = props.pattern[props.instrumentIndex].map((a, i) =>
@@ -7,6 +9,26 @@ const ControlView = (props) => {
     );
     props.ws.send(JSON.stringify({ type: "patternUpdate", data: newPattern }));
   };
+
+  const instrumentNames = props.instrument.includes("Bass")
+    ? [
+        "Bass (note 1)",
+        "Bass (note 2)",
+        "Bass (note 3)",
+        "Bass (note 4)",
+        "Bass (note 5)",
+        "Bass (note 6)",
+      ]
+    : props.instrument.includes("Melody")
+    ? [
+        "Melody (note 1)",
+        "Melody (note 2)",
+        "Melody (note 3)",
+        "Melody (note 4)",
+        "Melody (note 5)",
+        "Melody (note 6)",
+      ]
+    : ["Kick Drum", "Snare", "Closed Hi-Hat", "Cymbal", "Rimshot", "Clap"];
 
   return (
     <>
@@ -22,7 +44,8 @@ const ControlView = (props) => {
 
         <div id="matrix-control-container">
           <div
-            className={`h-auto w-14 p-2 bg-gray-400 flex flex-col items-center  justify-center self-start text-xs`}
+            style={{ marginLeft: "160px" }}
+            className={`h-auto w-14 p-2 bg-gray-400 flex flex-col items-center  justify-center self-start text-xs shadow rounded-t-sm`}
           >
             Beat 1
             <div
@@ -33,35 +56,49 @@ const ControlView = (props) => {
           </div>
           <div
             id="matrix-control"
-            className="h-auto w-auto flex flex-col items-center justify-center bg-gray-700 box-content"
+            className="h-auto w-auto flex flex-row items-center justify-center  box-content"
           >
-            {props.pattern.map((row, rowIndex) => (
-              <div
-                key={rowIndex}
-                className={`p-2 flex flex-row items-center justify-center space-x-2 ${
-                  rowIndex === props.instrumentIndex ? "bg-yellow-300" : ""
-                }`}
-              >
-                {row.map((col, colIndex) =>
-                  rowIndex === props.instrumentIndex ? (
-                    <div
-                      key={colIndex}
-                      className={`h-10 w-10 rounded-full cursor-pointer ${
-                        col === 1 ? "bg-blue-300" : "bg-gray-500"
-                      }`}
-                      onClick={() => updatePattern(colIndex)}
-                    ></div>
-                  ) : (
-                    <div
-                      key={colIndex}
-                      className={`h-10 w-10 rounded-full ${
-                        col === 1 ? "bg-blue-300" : "bg-gray-500"
-                      }`}
-                    ></div>
-                  )
-                )}
-              </div>
-            ))}
+            <div className="flex flex-col items-center justify-center">
+              {zip(instrumentNames, props.pattern).map(
+                ([name, row], rowIndex) => (
+                  <div
+                    id={`instrument-row-${rowIndex}`}
+                    className="h-10 w-36 p-2 flex flex-row items-center justify-end box-content"
+                  >
+                    {name}
+                  </div>
+                )
+              )}
+            </div>
+            <div className="flex flex-col items-center justify-center bg-gray-700 shadow rounded-sm">
+              {props.pattern.map((row, rowIndex) => (
+                <div
+                  key={rowIndex}
+                  className={`p-2 flex flex-row items-center justify-center space-x-2 ${
+                    rowIndex === props.instrumentIndex ? "bg-yellow-300" : ""
+                  }`}
+                >
+                  {row.map((col, colIndex) =>
+                    rowIndex === props.instrumentIndex ? (
+                      <div
+                        key={colIndex}
+                        className={`h-10 w-10 rounded-full cursor-pointer ${
+                          col === 1 ? "bg-blue-300" : "bg-gray-500"
+                        }`}
+                        onClick={() => updatePattern(colIndex)}
+                      ></div>
+                    ) : (
+                      <div
+                        key={colIndex}
+                        className={`h-10 w-10 rounded-full ${
+                          col === 1 ? "bg-blue-300" : "bg-gray-500"
+                        }`}
+                      ></div>
+                    )
+                  )}
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
